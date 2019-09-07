8c297e5f5d1761f169f8fd9980fa586520f44828
Sara Harvey <saraharveynyc@gmail.com>
2019-09-07 00:18:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+
 end
+
+
+#Set up a controller action that responds to a GET request to the route '/'. You'll want this action to render an erb file,index.erb.
